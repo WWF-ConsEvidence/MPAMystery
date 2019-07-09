4222243e85c91d56b42c17f23e67c81815738850
bauer784 <bauer784@users.noreply.github.com>
2019-07-09 17:30:27 -0400

    Adjusting FGID's and KII ID's to be coherent with master

--- 1_Data_wrangling/1_Social/1_QAQC/Flotim_Alor_Gov_AdjustID.R
+++ 1_Data_wrangling/1_Social/1_QAQC/Flotim_Alor_Gov_AdjustID.R
@@ -2,8 +2,8 @@
 # Amari Bauer, July 2019
 
 # ---- 1.1 Load libraries & data ----
-library(plyr)
 library(dplyr)
+library(plyr)
 library(openxlsx)
 library(chron)
 
@@ -46,17 +46,10 @@ Max_ID_per_table_master <- data.frame(FGD_MPA=230,
                                       KII_RIGHTS=3364,
                                       KII_ORG=176)
 
-# ---- 2.1 ALOR: Rename and add new IDs----
-
-Alor_FGD_Habitats <- Alor_FGD_Habitats %>%
-  rename(c("FGDID"="FGDCode")) %>%
-  .[order(.$HabitatID,.$FGDCode),] %>%
-  mutate(HabitatID=seq(Max_ID_per_table_master$FGD_HABITATS+1,Max_ID_per_table_master$FGD_HABITATS+length(HabitatID), by=1),
-         HabitatType=NA) %>%
-  select(HabitatID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,HabitatCode,HabitatTypeL,HabitatType)
+# ---- 2.1 ALOR: plyr::rename and add new IDs----
 
 Alor_FGD_MPA <- Alor_FGD_MPA %>%
-  rename(c("TraditionalGov"="TraditionalGovernance")) %>%
+  plyr::rename(c("TraditionalGov"="TraditionalGovernance")) %>%
   .[order(.$FGDID),] %>%
   mutate(Npenalty=NA, VerbalSanction=NA, PhysicalSanction=NA,MonetarySanction=NA,
          FGDID=seq(Max_ID_per_table_master$FGD_MPA+1,Max_ID_per_table_master$FGD_MPA+length(FGDID),by=1)) %>%
@@ -74,50 +67,53 @@ Alor_FGD_MPA <- Alor_FGD_MPA %>%
          ConTypeMarine,ConTypeGov,ConTypeUsers,ConTypeRec,ConTypeOther,ConTypeOther_SpecifyL,
          DataEntryCode,DataCheckCode,NotesL,QAQCNOtes)
 
+Alor_FGD_Habitats <- Alor_FGD_Habitats %>%
+  plyr::rename(c("FGDID"="FGDCode")) %>%
+  .[order(.$HabitatID,.$FGDCode),] %>%
+  mutate(HabitatID=seq(Max_ID_per_table_master$FGD_HABITATS+1,Max_ID_per_table_master$FGD_HABITATS+length(HabitatID), by=1),
+         HabitatType=NA, 
+         FGDCode=FGDCode+230) %>%
+  select(HabitatID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,HabitatCode,HabitatTypeL,HabitatType)
+
 Alor_FGD_Rules <- Alor_FGD_Rules %>%
-  rename(c("FGDID"="FGDCode")) %>%
+  plyr::rename(c("FGDID"="FGDCode")) %>%
   .[order(.$RuleID,.$FGDCode),] %>%
-  mutate(RuleID=seq(Max_ID_per_table_master$FGD_RULES+1,Max_ID_per_table_master$FGD_RULES+length(RuleID),by=1)) %>%
+  mutate(RuleID=seq(Max_ID_per_table_master$FGD_RULES+1,Max_ID_per_table_master$FGD_RULES+length(RuleID),by=1), 
+         FGDCode=FGDCode+230) %>%
   select(RuleID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,RuleCode,RuleDescriptionL) %>%
   filter(RuleID<837)
 
 Alor_FGD_Species <- Alor_FGD_Species %>%
-  rename(c("FGDID"="FGDCode")) %>%
+  plyr::rename(c("FGDID"="FGDCode")) %>%
   .[order(.$SpeciesID,.$FGDCode),] %>%
-  mutate(SpeciesID=seq(Max_ID_per_table_master$FGD_SPECIES+1,Max_ID_per_table_master$FGD_SPECIES+length(SpeciesID),by=1)) %>%
+  mutate(SpeciesID=seq(Max_ID_per_table_master$FGD_SPECIES+1,Max_ID_per_table_master$FGD_SPECIES+length(SpeciesID),by=1), 
+         FGDCode=FGDCode+230) %>%
   select(SpeciesID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,Species,SpeciesLFamily,SpeciesLGenus,
          SpeciesLSpecies,SpeciesEnglish) %>%
   filter(SpeciesID < 1447)
 
 Alor_FGD_Stakeholders <- Alor_FGD_Stakeholders %>%
-  rename(c("Stakeh0lderID"="StakeholderID","FGDID"="FGDCode","C0untryC0de"="CountryCode","SiteC0de"="SiteCode",
+  plyr::rename(c("Stakeh0lderID"="StakeholderID","FGDID"="FGDCode","C0untryC0de"="CountryCode","SiteC0de"="SiteCode",
            "SettlementC0de"="SettlementCode")) %>%
   .[order(.$StakeholderID,.$FGDCode),] %>%
-  mutate(StakeholderID=seq(Max_ID_per_table_master$FGD_STAKEHOLDERS+1,Max_ID_per_table_master$FGD_STAKEHOLDERS+length(StakeholderID),by=1)) %>%
+  mutate(StakeholderID=seq(Max_ID_per_table_master$FGD_STAKEHOLDERS+1,Max_ID_per_table_master$FGD_STAKEHOLDERS+length(StakeholderID),by=1), 
+         FGDCode=FGDCode+230) %>%
   select(StakeholderID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,StakeholderNameL,
          ParticipateEstablish,ParticipateBoundaries,ParticipateAdmin,ParticipateRules,
          MonitorEco,MonitorSoc,MonitorCompliance,EnforceFreq) %>%
   filter(StakeholderID < 1862)
 
 Alor_FGD_Users <- Alor_FGD_Users %>%
-  rename(c("FGDID"="FGDCODE")) %>%
+  plyr::rename(c("FGDID"="FGDCODE")) %>%
   .[order(.$USERID,.$FGDCODE),] %>%
-  mutate(USERID=seq(Max_ID_per_table_master$FGD_USERS+1,Max_ID_per_table_master$FGD_USERS+length(USERID),by=1)) %>%
+  mutate(USERID=seq(Max_ID_per_table_master$FGD_USERS+1,Max_ID_per_table_master$FGD_USERS+length(USERID),by=1), 
+         FGDCODE=FGDCODE+230) %>%
   select(USERID,FGDCODE,YEAR,CountryCode,SiteCode,SettlementCode,UserCode,UserNameL,UserNameE,ExtBnd,
          IntBnd,ParticipateEstablish,ParticipateBoundaries,ParticipateAdmin,ParticipateRules,MonitorEco,
          MonitorSoc,MonitorCompliance,EnforceFreq,ContributionRank,BenefitRank)
 
-Alor_KII_habrules <- Alor_KII_habrules %>%
-  rename(c("KII_ID"="KIICode")) %>%
-  .[order(.$HABRulesID,.$KIICode),] %>%
-  mutate(HabSpecRule=NA,
-         HABRulesID=seq(Max_ID_per_table_master$KII_habRULES+1,Max_ID_per_table_master$KII_habRULES+length(HABRulesID),by=1)) %>%
-  select(HABRulesID,KIICode,Year,CountryCode,SiteCode,SettlementCode,HabitatNameL,HabRule,HabSpecRuleL,
-         HabSpecRule) %>%
-  filter(HABRulesID < 1605)
-
 Alor_KII_MPA <- Alor_KII_MPA %>%
-  rename(c("KII_Field_Code"="KIICode","INTERVIEW.MONTH"="INTERVIEW MONTH","INTERVIEW.YEAR"="INTERVIEW YEAR")) %>%
+  plyr::rename(c("KII_Field_Code"="KIICode","INTERVIEW.MONTH"="INTERVIEW MONTH","INTERVIEW.YEAR"="INTERVIEW YEAR")) %>%
   .[order(.$KII_ID),] %>%
   mutate(`INTERVIEW DATE`=NA,
          KII_ID=seq(Max_ID_per_table_master$KII_MPA+1,Max_ID_per_table_master$KII_MPA+length(KII_ID),by=1)) %>%
@@ -137,16 +133,28 @@ Alor_KII_MPA <- Alor_KII_MPA %>%
          AnyOtherInfoL,AnyOtherKIL,AnyOtherDocsL,NotesL,DataEntry,DataCheck,ViolationFreq) %>%
   filter(KII_ID < 506)
 
+Alor_KII_habrules <- Alor_KII_habrules %>%
+  plyr::rename(c("KII_ID"="KIICode")) %>%
+  .[order(.$HABRulesID,.$KIICode),] %>%
+  mutate(HabSpecRule=NA,
+         KIICode=KIICode+424,
+         HABRulesID=seq(Max_ID_per_table_master$KII_habRULES+1,Max_ID_per_table_master$KII_habRULES+length(HABRulesID),by=1)) %>%
+  select(HABRulesID,KIICode,Year,CountryCode,SiteCode,SettlementCode,HabitatNameL,HabRule,HabSpecRuleL,
+         HabSpecRule) %>%
+  filter(HABRulesID < 1605)
+
 Alor_KII_Org <- Alor_KII_Org %>%
   .[order(.$OrgID,.$KII_ID),] %>%
-  mutate(OrgID=seq(Max_ID_per_table_master$KII_ORG+1,Max_ID_per_table_master$KII_ORG+length(OrgID),by=1)) %>%
+  mutate(OrgID=seq(Max_ID_per_table_master$KII_ORG+1,Max_ID_per_table_master$KII_ORG+length(OrgID),by=1),
+         KII_ID=KII_ID+424) %>%
   select(OrgID,KII_ID,YEAR,CountryCode,SiteCode,SettlementCode,OrganizationNameL)																			
 
 Alor_KII_Rights <- Alor_KII_Rights %>%
-  rename(c("KII_ID"="KIICode")) %>%
+  plyr::rename(c("KII_ID"="KIICode")) %>%
   .[order(.$RightsID,.$KIICode),] %>%
   mutate(ISSUES=NA,
          UserSpecRule=NA,
+         KIICode=KIICode+424,
          RightsID=seq(Max_ID_per_table_master$KII_RIGHTS+1,Max_ID_per_table_master$KII_RIGHTS+length(RightsID),by=1)) %>%
   select(RightsID,KIICode,YEAR,CountryCode,SiteCode,SettlementCode,UserNameL,UserRule,UserSpecRuleL,
          UserSpecRule,GovtSupport,UserRulesInc,ISSUES) %>%
@@ -154,37 +162,29 @@ Alor_KII_Rights <- Alor_KII_Rights %>%
 
 
 Alor_KII_SppRules <- Alor_KII_SppRules %>%
-  rename(c("KII_ID"="KIICode","SpeciesLFamily"="Family","SpeciesLGenus"="Genus",
+  plyr::rename(c("KII_ID"="KIICode","SpeciesLFamily"="Family","SpeciesLGenus"="Genus",
            "SpeciesLSpecies"="Species")) %>%
   .[order(.$SPPRulesID,.$KIICode),] %>%
-  mutate(KII_order=NA, SppSpecificRule=NA,
+  mutate(KII_order=NA, SppSpecificRule=NA,KIICode=KIICode+424,
          SPPRulesID=seq(Max_ID_per_table_master$KII_sppRULES+1,Max_ID_per_table_master$KII_sppRULES+length(SPPRulesID),by=1)) %>%
   select(SPPRulesID,KIICode,YEAR,CountryCode,SiteCode,SettlementCode,KII_order,SpeciesNameL,Family,
          Genus,Species,SppRule,SppSpecificRuleL,SppSpecificRule) %>%
   filter(SPPRulesID < 2694)
 
 Alor_KII_Zones <- Alor_KII_Zones %>%
-  rename(c("KII_ID"="KIICode")) %>%
+  plyr::rename(c("KII_ID"="KIICode")) %>%
   .[order(.$ZoneID,.$KIICode),] %>%
-  mutate(KIIOrder=NA,
+  mutate(KIIOrder=NA, KIICode=KIICode+424,
          ZoneID=seq(Max_ID_per_table_master$KII_ZONES+1,Max_ID_per_table_master$KII_ZONES+length(ZoneID),by=1)) %>%
   select(ZoneID,KIICode,YEAR,Country,SiteCode,SettlementCode,KIIOrder,ZoneTypeL,ZoneQuantity,ZoneOrg,
          ZoneCoord) %>%
   filter(ZoneID < 878)
 
-# ---- 2.2 FLOTIM: Rename and add new IDs----
-
-Flotim_FGD_Habitats <- Flotim_FGD_Habitats %>%
-  rename(c("FGDID"="FGDCode")) %>%
-  .[order(.$HabitatID,.$FGDCode),] %>%
-  mutate(HabitatID=seq(max(Alor_FGD_Habitats$HabitatID)+1,max(Alor_FGD_Habitats$HabitatID)+length(HabitatID),by=1),
-         HabitatType=NA) %>%
-  select(HabitatID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,HabitatCode,HabitatTypeL,HabitatType) %>%
-  filter(HabitatID < 1020)
+# ---- 2.2 FLOTIM: plyr::rename and add new IDs----
 
 Flotim_FGD_MPA <- Flotim_FGD_MPA %>%
-  rename(c("TraditionalGov"="TraditionalGovernance","FGroundTime..minute."="FGroundTime",
-           "FGroundDist..kilometer."="FGroundDist","FGroundSize..kilometer.square."="FGroundSize")) %>%
+  plyr::rename(c("TraditionalGov"="TraditionalGovernance","FGroundTime..minute."="FGroundTime",
+                 "FGroundDist..kilometer."="FGroundDist","FGroundSize..kilometer.square."="FGroundSize")) %>%
   .[order(.$FGDID,.$FGDCode),] %>%
   mutate(Npenalty=NA, VerbalSanction=NA, PhysicalSanction=NA,MonetarySanction=NA,
          FGDID=seq(max(Alor_FGD_MPA$FGDID)+1,max(Alor_FGD_MPA$FGDID)+length(FGDID),by=1)) %>%
@@ -203,53 +203,57 @@ Flotim_FGD_MPA <- Flotim_FGD_MPA %>%
          DataEntryCode,DataCheckCode,NotesL,QAQCNOtes) %>%
   filter(FGDID < 284)
 
+Flotim_FGD_Habitats <- Flotim_FGD_Habitats %>%
+  plyr::rename(c("FGDID"="FGDCode")) %>%
+  .[order(.$HabitatID,.$FGDCode),] %>%
+  mutate(HabitatID=seq(max(Alor_FGD_Habitats$HabitatID)+1,max(Alor_FGD_Habitats$HabitatID)+length(HabitatID),by=1),
+         HabitatType=NA,
+         FGDCode=FGDCode+283) %>%
+  select(HabitatID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,HabitatCode,HabitatTypeL,HabitatType) %>%
+  filter(HabitatID < 1020)
+
 Flotim_FGD_Rules <- Flotim_FGD_Rules %>%
-  rename(c("FGDID"="FGDCode")) %>%
+  plyr::rename(c("FGDID"="FGDCode")) %>%
   .[order(.$RuleID,.$FGDCode),] %>%
-  mutate(RuleID=seq(max(Alor_FGD_Rules$RuleID)+1,(max(Alor_FGD_Rules$RuleID)+length(RuleID)),by=1)) %>%
+  mutate(RuleID=seq(max(Alor_FGD_Rules$RuleID)+1,(max(Alor_FGD_Rules$RuleID)+length(RuleID)),by=1),
+         FGDCode=FGDCode+283) %>%
   select(RuleID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,RuleCode,RuleDescriptionL) %>%
   filter(RuleID < 863)
 
 Flotim_FGD_Species <- Flotim_FGD_Species %>%
-  rename(c("FGDID"="FGDCode")) %>%
+  plyr::rename(c("FGDID"="FGDCode")) %>%
   .[order(.$SpeciesID,.$FGDCode),] %>%
-  mutate(SpeciesID=seq(max(Alor_FGD_Species$SpeciesID)+1,max(Alor_FGD_Species$SpeciesID)+length(SpeciesID),by=1)) %>%
+  mutate(SpeciesID=seq(max(Alor_FGD_Species$SpeciesID)+1,max(Alor_FGD_Species$SpeciesID)+length(SpeciesID),by=1),
+         FGDCode=FGDCode+283) %>%
   select(SpeciesID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,Species,SpeciesLFamily,SpeciesLGenus,
          SpeciesLSpecies,SpeciesEnglish) %>%
   filter(SpeciesID < 1571)
 
 Flotim_FGD_Stakeholders <- Flotim_FGD_Stakeholders %>%
-  rename(c("Stakeh0lderID"="StakeholderID","FGDID"="FGDCode","CountryC0de"="CountryCode","SiteC0de"="SiteCode",
+  plyr::rename(c("Stakeh0lderID"="StakeholderID","FGDID"="FGDCode","CountryC0de"="CountryCode","SiteC0de"="SiteCode",
            "SettlementC0de"="SettlementCode")) %>%
   .[order(.$StakeholderID,.$FGDCode),] %>%
-  mutate(StakeholderID=seq(max(Alor_FGD_Stakeholders$StakeholderID)+1,max(Alor_FGD_Stakeholders$StakeholderID)+length(StakeholderID),by=1)) %>%
+  mutate(StakeholderID=seq(max(Alor_FGD_Stakeholders$StakeholderID)+1,max(Alor_FGD_Stakeholders$StakeholderID)+length(StakeholderID),by=1),
+         FGDCode=FGDCode+283) %>%
   select(StakeholderID,FGDCode,YEAR,CountryCode,SiteCode,SettlementCode,StakeholderNameL,
          ParticipateEstablish,ParticipateBoundaries,ParticipateAdmin,ParticipateRules,
          MonitorEco,MonitorSoc,MonitorCompliance,EnforceFreq) %>%
   filter(StakeholderID < 2044)
 
 Flotim_FGD_Users <- Flotim_FGD_Users %>%
-  rename(c("FGDID"="FGDCODE")) %>%
+  plyr::rename(c("FGDID"="FGDCODE")) %>%
   .[order(.$USERID,.$FGDCODE),] %>%
-  mutate(USERID=seq(max(Alor_FGD_Users$USERID)+1,max(Alor_FGD_Users$USERID)+length(USERID),by=1)) %>%
+  mutate(USERID=seq(max(Alor_FGD_Users$USERID)+1,max(Alor_FGD_Users$USERID)+length(USERID),by=1),
+         FGDCODE=FGDCODE+283) %>%
   select(USERID,FGDCODE,YEAR,CountryCode,SiteCode,SettlementCode,UserCode,UserNameL,UserNameE,ExtBnd,
          IntBnd,ParticipateEstablish,ParticipateBoundaries,ParticipateAdmin,ParticipateRules,MonitorEco,
          MonitorSoc,MonitorCompliance,EnforceFreq,ContributionRank,BenefitRank) %>%
   filter(USERID < 1665)
 
-Flotim_KII_habrules <- Flotim_KII_habrules %>%
-  rename(c("KII_ID"="KIICode")) %>%
-  .[order(.$HABRulesID,.$KIICode),] %>%
-  mutate(HabSpecRule=NA,
-         HABRulesID=seq(max(Alor_KII_habrules$HABRulesID)+1,max(Alor_KII_habrules$HABRulesID)+length(HABRulesID),by=1)) %>%
-  select(HABRulesID,KIICode,Year,CountryCode,SiteCode,SettlementCode,HabitatNameL,HabRule,HabSpecRuleL,
-         HabSpecRule) %>%
-  filter(HABRulesID < 1827)
-
 Flotim_KII_MPA <- Flotim_KII_MPA %>%
-  rename(c("KII_Field_Code"="KIICode","INTERVIEW.MONTH"="INTERVIEW MONTH","INTERVIEW.YEAR"="INTERVIEW YEAR")) %>%
+  plyr::rename(c("KII_Field_Code"="KIICode","INTERVIEW.MONTH"="INTERVIEW MONTH","INTERVIEW.YEAR"="INTERVIEW YEAR")) %>%
   .[order(.$KII_ID),] %>%
-  mutate(`INTERVIEW DATE`=NA,
+  mutate(`INTERVIEW DATE`=NA, 
          KII_ID=seq(max(Alor_KII_MPA$KII_ID)+1,max(Alor_KII_MPA$KII_ID)+length(KII_ID),by=1)) %>%
   select(KII_ID,CountryCode,SiteCode,SettlementCode,KIICode,RefFGDCode,InformantName,KeyInformantRole,
          InterviewerCode,NotetakerCode,InterviewDate,`INTERVIEW DATE`,`INTERVIEW MONTH`,`INTERVIEW YEAR`,
@@ -267,16 +271,27 @@ Flotim_KII_MPA <- Flotim_KII_MPA %>%
          AnyOtherInfoL,AnyOtherKIL,AnyOtherDocsL,NotesL,DataEntry,DataCheck,ViolationFreq) %>%
   filter(KII_ID < 584)
 
+Flotim_KII_habrules <- Flotim_KII_habrules %>%
+  plyr::rename(c("KII_ID"="KIICode")) %>%
+  .[order(.$HABRulesID,.$KIICode),] %>%
+  mutate(HabSpecRule=NA, KIICode=KIICode+505,
+         HABRulesID=seq(max(Alor_KII_habrules$HABRulesID)+1,max(Alor_KII_habrules$HABRulesID)+length(HABRulesID),by=1)) %>%
+  select(HABRulesID,KIICode,Year,CountryCode,SiteCode,SettlementCode,HabitatNameL,HabRule,HabSpecRuleL,
+         HabSpecRule) %>%
+  filter(HABRulesID < 1827)
+
 Flotim_KII_Org <- Flotim_KII_Org %>%
   .[order(.$OrgID,.$KII_ID),] %>%
-  mutate(OrgID=seq(max(Alor_KII_Org$OrgID)+1,max(Alor_KII_Org$OrgID)+length(OrgID),by=1)) %>%
+  mutate(OrgID=seq(max(Alor_KII_Org$OrgID)+1,max(Alor_KII_Org$OrgID)+length(OrgID),by=1),
+         KII_ID=KII_ID+505) %>%
   select(OrgID,KII_ID,YEAR,CountryCode,SiteCode,SettlementCode,OrganizationNameL)	
 
 Flotim_KII_Rights <- Flotim_KII_Rights %>%
-  rename(c("KII_ID"="KIICode")) %>%
+  plyr::rename(c("KII_ID"="KIICode")) %>%
   .[order(.$RightsID,.$KIICode),] %>%
   mutate(ISSUES=NA,
          UserSpecRule=NA,
+         KIICode=KIICode+505,
          RightsID=seq(max(Alor_KII_Rights$RightsID)+1,max(Alor_KII_Rights$RightsID)+length(RightsID),by=1)) %>%
   select(RightsID,KIICode,YEAR,CountryCode,SiteCode,SettlementCode,UserNameL,UserRule,UserSpecRuleL,
          UserSpecRule,GovtSupport,UserRulesInc,ISSUES) %>%
@@ -284,19 +299,19 @@ Flotim_KII_Rights <- Flotim_KII_Rights %>%
 
 
 Flotim_KII_SppRules <- Flotim_KII_SppRules %>%
-  rename(c("KII_ID"="KIICode","SpeciesLFamily"="Family","SpeciesLGenus"="Genus",
+  plyr::rename(c("KII_ID"="KIICode","SpeciesLFamily"="Family","SpeciesLGenus"="Genus",
            "SpeciesLSpecies"="Species")) %>%
   .[order(.$SPPRulesID,.$KIICode),] %>%
-  mutate(KII_order=NA, SppSpecificRule=NA,
+  mutate(KII_order=NA, SppSpecificRule=NA,  KIICode=KIICode+505,
          SPPRulesID=seq(max(Alor_KII_SppRules$SPPRulesID)+1,max(Alor_KII_SppRules$SPPRulesID)+length(SPPRulesID),by=1)) %>%
   select(SPPRulesID,KIICode,YEAR,CountryCode,SiteCode,SettlementCode,KII_order,SpeciesNameL,Family,
          Genus,Species,SppRule,SppSpecificRuleL,SppSpecificRule) %>%
   filter(SPPRulesID < 3066)
 
 Flotim_KII_Zones <- Flotim_KII_Zones %>%
-  rename(c("KII_ID"="KIICode")) %>%
+  plyr::rename(c("KII_ID"="KIICode")) %>%
   .[order(.$ZoneID,.$KIICode),] %>%
-  mutate(KIIOrder=NA,
+  mutate(KIIOrder=NA, KIICode=KIICode+505,
          ZoneID=seq(max(Alor_KII_Zones$ZoneID)+1,max(Alor_KII_Zones$ZoneID)+length(ZoneID),by=1)) %>%
   select(ZoneID,KIICode,YEAR,Country,SiteCode,SettlementCode,KIIOrder,ZoneTypeL,ZoneQuantity,ZoneOrg,
          ZoneCoord) %>%
