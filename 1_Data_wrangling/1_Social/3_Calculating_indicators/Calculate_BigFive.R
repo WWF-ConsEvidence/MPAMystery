d0d85bc4369c48d8c8037372ef45e568f7d6205b
bauer784 <bauer784@users.noreply.github.com>
2019-07-30 17:23:24 -0400

    Eliminating some unnecessary lines

--- 1_Data_wrangling/1_Social/3_Calculating_indicators/Calculate_BigFive.R
+++ 1_Data_wrangling/1_Social/3_Calculating_indicators/Calculate_BigFive.R
@@ -6,14 +6,31 @@
 # modified: October 2017
 # 
 # ---- inputs ----
-# source_social_data R script
+# KC_HHData_ForMPAMystery -- in BHS Master Database in Access
+#   -this table can be created by running the query saved in the 
+#    'KC_CodingSubsettingAnalysis' Group, named 'Q_HHData_ForMPAMystery'.
+#   -this table is the cleaned data from all MPAs and years, ready to be analyzed
+#    and calculated into the food security, material assets, place attachment, marine
+#    tenure, and school enrollment indexes -- along with other livelihood and household 
+#    demographic variables.
+# KC_IndDemos_ForMPAMystery -- in BHS Master Database in Access
+#   -the IndoDemos table can be created by running the query saved in the 
+#    'KC_CodingSubsettingAnalysis' Group, named 'Q_IndividualDemos_ForMPAMystery'.
+#   -the IndDemos table is the cleaned data from all MPAs and years, ready to be analyzed
+#    and calculate the school enrollment rate, days unwell, and head of household gender. 
+# HH_tbl_SETTLEMENT -- in BHS Master Database in Access
+#   -the Settlement table provides the names and treatment vs. non-treatment status for 
+#    each settlementID.
+# !!!!!! TO FIX !!!!!!!!!!!!!! ADD: Ethnicity
 # 
 # 
 # ---- code sections ----
-#  1) "Big Five" Indexes
-#  2) Derive Additional Variables
+#  1) Import Data, and Subset
+#  2) "Big Five" Indexes
+#  3) Technical Report Datasets, Proportional Data
+#  4) MPA Impact Summary/Technical Report Introduction Stats
+#  5) Define Global Plot Themes and Guides 
 # 
-<<<<<<< HEAD
 #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
@@ -21,13 +38,14 @@
 #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
-
-pacman::p_load(reshape2,ggplot2,dplyr)
+#pacman::p_load(reshape2,ggplot2,dplyr)
+library(plyr)
+library(dplyr)
 library(reshape2)
+library(ggplot2)
 # ---- 1.1 Import data ----
-
 # FROM FLAT FILE
-source('1_Data_wrangling/1_Social/2_Source_data/Source_social_data_flat_files.R')
+source("C:/Users/HP/Dropbox/NotThisOne/Source_social_data_flat_files.R")
 
 
 # Whichever option you chose above, you will still need to upload Ethnicity data from a flat file
@@ -41,18 +59,15 @@ source('1_Data_wrangling/1_Social/2_Source_data/Source_social_data_flat_files.R'
 
 
 
-=======
->>>>>>> be759c972146e724272ddd4afb4c716579033e14
 # 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
-# ---- SECTION 1: "Big Five" Indexes ----
+# ---- SECTION 2: "Big Five" Indexes ----
 #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # 
 
-# ---- 1.1 Compute indexes ----
-
+# ---- 2.1 Compute indexes ----
 HHData$MAIndex <- ifelse(HHData$RemoveMA=="No",
                      rowSums(HHData[,c("CarTruck", "Bicycle", "Motorcycle", "BoatNoMotor", "BoatOutboard", 
                                    "BoatInboard", "PhoneCombined", "TV", "Entertain", "Satellite", 
@@ -78,37 +93,29 @@ HHData$FSIndex <-
                                         "FreqAdultSkip", "Hungry")],
                               na.rm=TRUE),
                       NA)) %>%
-<<<<<<< HEAD
-  plyr::revalue(., c("0"="0", "1"="2.04","2"="2.99","3"="3.77","4"="4.5","5"="5.38","6"="6.06"))
-=======
-  recode(., "0"="0", "1"="2.04","2"="2.99","3"="3.77","4"="4.5","5"="5.38","6"="6.06")
-
-# This line will reverse the scale so that larger values indicate more Food Secure
-
->>>>>>> be759c972146e724272ddd4afb4c716579033e14
+  revalue(., c("0"="0", "1"="2.04","2"="2.99","3"="3.77","4"="4.5","5"="5.38","6"="6.06"))
 HHData$FSIndex <- 6.06-as.numeric(HHData$FSIndex) 
-
-
+detach(package:plyr)
 HHData <- 
   IndDemos %>%
-  group_by(HouseholdID) %>%
-  summarise(NumberChild=sum(SchoolAge,na.rm=T),
-            NumberEnrolled=sum(ChildEnrolled,na.rm=T),
-            PercentEnrolled=ifelse(NumberChild!=0 & !is.na(NumberEnrolled),
-                                   as.character(round((NumberEnrolled/NumberChild)*100,2)),
-                                   ifelse(NumberChild==0,
-                                          "No School-Aged Children","No Data")),
-            SERate=ifelse(NumberChild!=0 & !is.na(NumberEnrolled),
-                          round((NumberEnrolled/NumberChild),2),
-                          NA)) %>%
+          group_by(HouseholdID) %>%
+           summarise(NumberChild=sum(SchoolAge,na.rm=T),
+                     NumberEnrolled=sum(ChildEnrolled,na.rm=T),
+                     PercentEnrolled=ifelse(NumberChild!=0 & !is.na(NumberEnrolled),
+                                            as.character(round((NumberEnrolled/NumberChild)*100,2)),
+                                            ifelse(NumberChild==0,
+                                                   "No School-Aged Children","No Data")),
+                     SERate=ifelse(NumberChild!=0 & !is.na(NumberEnrolled),
+                                   round((NumberEnrolled/NumberChild),2),
+                                   NA)) %>%
   left_join(HHData,.,by="HouseholdID")
   
 
-# ---- 1.2 Define "Big Five" data frame - averaged by settlement, for each monitoring year ----
+# ---- 2.2 Define "Big Five" data frame - averaged by settlement, for each monitoring year ----
 
 BigFive.SettleGroup <- 
   HHData %>%
-  group_by(SettlementID,SettlementName,MPAID,Treatment,MonitoringYear) %>%
+  group_by(SettlementID,SettlementName,MPAID,MonitoringYear,Treatment) %>%
   summarise(FSMean=round(mean(FSIndex,na.rm=T),2),
             FSErr=round(sd(FSIndex,na.rm=T)/sqrt(length(FSIndex)),2),
             MAMean=round(mean(MAIndex,na.rm=T),2),
@@ -118,12 +125,11 @@ BigFive.SettleGroup <-
             MTMean=round(mean(MTIndex,na.rm=T),2),
             MTErr=round(sd(MTIndex,na.rm=T)/sqrt(length(MTIndex)),2),
             SEMean=round(mean(SERate,na.rm=T),2),
-<<<<<<< HEAD
             SEErr=round(sd(SERate,na.rm=T)/sqrt(length(SERate)),2)) %>%
   na.omit()
 
-#BigFive.SettleGroup <- rbind.data.frame(BigFive.SettleGroup,
-                                        data.frame(SettlementID=c(116:228),
+BigFive.SettleGroup <- rbind.data.frame(BigFive.SettleGroup,
+                                        data.frame(SettlementID=c(72,104:112,113:115),
                                                    SettlementName=c(as.character(unique(BigFive.SettleGroup$SettlementName[BigFive.SettleGroup$SettlementID==72])),
                                                                     as.character(unique(BigFive.SettleGroup$SettlementName[BigFive.SettleGroup$MPAID==2 &
                                                                                                                              BigFive.SettleGroup$Treatment==1 &
@@ -133,12 +139,9 @@ BigFive.SettleGroup <-
                                                    MPAID=c(5,rep(2,9),rep(3,3)),Treatment=rep(1,13),MonitoringYear=rep("Baseline",13),
                                                    as.data.frame(matrix(rep(NA,10),ncol=10,nrow=13,
                                                                         dimnames=list(NULL,colnames(BigFive.SettleGroup[6:15]))))))
-=======
-            SEErr=round(sd(SERate,na.rm=T)/sqrt(length(SERate)),2))
->>>>>>> be759c972146e724272ddd4afb4c716579033e14
 
 
-# ---- 1.3 Define "Big Five" data frame - averaged by control group (per MPA), for each monitoring year ----
+# ---- 2.4 Define "Big Five" data frame - averaged by control group (per MPA), for each monitoring year ----
 
 BigFive.ControlGroup <- 
   HHData %>%
@@ -153,10 +156,17 @@ BigFive.ControlGroup <-
             MTMean=round(mean(MTIndex,na.rm=T),2),
             MTErr=round(sd(MTIndex,na.rm=T)/sqrt(length(MTIndex)),2),
             SEMean=round(mean(SERate,na.rm=T),2),
-            SEErr=round(sd(SERate,na.rm=T)/sqrt(length(SERate)),2))
+            SEErr=round(sd(SERate,na.rm=T)/sqrt(length(SERate)),2)) %>%
+  na.omit()
 
+BigFive.ControlGroup <- 
+  cbind.data.frame("SettlementID"=rep.int(NA,nrow(unique(BigFive.SettleGroup[c("MPAID","MonitoringYear")]))),
+                   "SettlementName"=rep.int("Control",nrow(unique(BigFive.SettleGroup[c("MPAID","MonitoringYear")]))),
+                   BigFive.ControlGroup[,"MPAID"],
+                   "Treatment"=rep.int(0,nrow(unique(BigFive.SettleGroup[c("MPAID","MonitoringYear")]))),
+                   BigFive.ControlGroup[,2:12])
 
-# ---- 1.4 Define "Big Five" data frame - averaged by MPA, for each monitoring year ----
+# ---- 2.5 Define "Big Five" data frame - averaged by MPA, for each monitoring year ----
 
 BigFive.MPAGroup <- 
   HHData %>%
@@ -171,8 +181,15 @@ BigFive.MPAGroup <-
             MTMean=round(mean(MTIndex,na.rm=T),2),
             MTErr=round(sd(MTIndex,na.rm=T)/sqrt(length(MTIndex)),2),
             SEMean=round(mean(SERate,na.rm=T),2),
-            SEErr=round(sd(SERate,na.rm=T)/sqrt(length(SERate)),2))
+            SEErr=round(sd(SERate,na.rm=T)/sqrt(length(SERate)),2)) %>%
+  na.omit()
 
+BigFive.MPAGroup <-
+  cbind.data.frame("SettlementID"=rep.int(NA,nrow(unique(BigFive.SettleGroup[c("MPAID","MonitoringYear")]))),
+                   "SettlementName"=rep.int("MPA",nrow(unique(BigFive.SettleGroup[c("MPAID","MonitoringYear")]))),
+                   BigFive.MPAGroup[,"MPAID"],
+                   "Treatment"=rep.int(1,nrow(unique(BigFive.SettleGroup[c("MPAID","MonitoringYear")]))),
+                   BigFive.MPAGroup[,2:12])
 
 # 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -188,11 +205,11 @@ HHData$cFS
 #Summing child food variables to construct child food security index on the household level
 HHData$cFS <-
   ifelse(HHData$RemoveFS=="No",
-                     rowSums(HHData[,c("LowCostFood", "ChildBalancedMeal", "ChildNotEnough", 
-                                       "ChildPortion", "ChildHungry", "ChildSkip", "FreqChildSkip", 
-                                       "NoMealChild")],
-                             na.rm=TRUE),
-                     NA)
+         rowSums(HHData[,c("LowCostFood", "ChildBalancedMeal", "ChildNotEnough", 
+                           "ChildPortion", "ChildHungry", "ChildSkip", "FreqChildSkip", 
+                           "NoMealChild")],
+                 na.rm=TRUE),
+         NA)
 
 #Adding a column that classifies whether they meet the standard for child food insecurity 
 HHData$cat.cFS <- ifelse(HHData$cFS>=6.9,"Evidence",
@@ -205,8 +222,8 @@ HHData$cat.cFS <- ifelse(HHData$cFS>=6.9,"Evidence",
 #  adding a fisher dummy variable, calculating HH size, days unwell per individual
 HHData <- 
   right_join(HHData,IndDemos[IndDemos$RelationHHH==0 &
-                                       !is.na(IndDemos$RelationHHH),c("HouseholdID","IndividualGender",
-                                                                      "IndividualEducation","IndividualAge")],by="HouseholdID") %>%
+                               !is.na(IndDemos$RelationHHH),c("HouseholdID","IndividualGender",
+                                                              "IndividualEducation","IndividualAge")],by="HouseholdID") %>%
   mutate(InterviewYear=factor(InterviewYear,
                               levels=c("2010","2011","2012","2013","2014","2015","2016","2017","2018",
                                        "2019","2020","2021","2022","2023","2024","2025","2026","2027",
@@ -218,3 +235,4 @@ HHData <-
                        }),
          fisher=ifelse(PrimaryLivelihood==3 | SecondaryLivelihood==3 | TertiaryLivelihood==3,1,0)) %>%
   left_join(.,(IndDemos %>% group_by(HouseholdID) %>% summarise(DaysUnwell=sum(DaysUnwell,na.rm=T)/length(HouseholdID))), by="HouseholdID")
+  
