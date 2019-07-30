f68902264e34388881de39974269c9680016f1a2
Kelly Claborn <clabornkelly@gmail.com>
2019-07-30 11:35:44 +0700

    added notes to calculate_trends.R for MPAID reference

--- Calculate_trends.R
+++ Calculate_trends.R
@@ -10,4 +10,24 @@
 
 source('2_Functions/Function_calculate_trends.R')
 
-mpa.trends(MPA=15)
+mpa.trends(MPA=)
+
+
+# For your notes:
+
+# -- Bird's Head Seascape
+# MPAID 1 = Teluk Mayalibit
+# MPAID 2 = Teluk Cenderawasih
+# MPAID 3 = Kaimana
+# MPAID 4 = Kofiau dan Pulau Boo
+# MPAID 5 = Selat Dampier
+# MPAID 6 = Misool Selatan Timur
+
+# -- Sunda Banda Seascape
+# MPAID 15 = Selat Pantar
+# MPAID 16 = Flores Timur
+# MPAID 17 = Kei Kecil
+# MPAID 18 = Koon
+# MPAID 19 = Yamdena
+# MPAID 20 = Sulawesi Tenggara
+# MPAID 21 = Wakatobi
