fa35fdd55c08f1336d03a0430c82d38558f5e80e
mdenardo <matheusdenardo@gmail.com>
2020-07-07 09:52:32 -0400

    added notes on household api querying for MPASocial

--- 1_Data_wrangling/1_Social/2_Source_data/Connecting_to_MPASocial_API.R
+++ 1_Data_wrangling/1_Social/2_Source_data/Connecting_to_MPASocial_API.R
@@ -14,7 +14,7 @@ pacman::p_load(httr, jsonlite)
 # ---- 1.1 create objects for API endpoint URL ----
 
 base <- "https://mpasocial.org/api/" # this is the api base url
-endpoint <- "death" # this is the name of the api endpoint where you specify the database subtable you want to access
+endpoint <- "household" # this is the name of the api endpoint where you specify the database subtable you want to access
 
 # Names of functional API endpoints as of July 1, 2020: 
 # household, demographic, birth, death, 
@@ -28,7 +28,11 @@ url <- paste(base,endpoint, sep = "")
 
 # ---- 1.3 Optional querying of household data only ----
 
-# Can optionally filter "MPA"  and/or "InterviewYear" fields in "household" api endpoint call
+# Can optionally filter by "mpa"  and/or "interviewyear" fields in "household" api endpoint call
+# Decreases processing time
+
+# query <- "?mpa=4&interviewyear=2013" # only applies to "household" api endpoint and can only filter by mpa or interviewer
+# url <- paste(base,endpoint, query, sep = "") # url containing query
 
 # Examples of query syntax:
 # "https://mpasocial.org/api/household?MPA=1,3,15:18&interviewyear=2019")
